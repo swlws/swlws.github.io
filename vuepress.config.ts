9c8096d5829639779cfb98d0b012cf13264d9360
swlws <979623074@qq.com>
2022-01-26 11:36:39 +0800

    feat: logo

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -7,11 +7,13 @@ export default defineUserConfig({
   base: "/",
   title: "CodeLife",
   description: "JUST PLAY",
+  head: [["link", { rel: "icon", href: "/imgs/logo.png" }]],
   lang: "zh-CN",
   plugins,
   theme: "@vuepress/theme-default",
   themeConfig: {
     repo: "https://github.com/swlws",
+    logo: "/imgs/logo.png",
     navbar,
     sidebar,
     // 设置根据页面标题自动生成的侧边栏的最大深度。
